1c2ae8682d2c9a53ae99ae324cb6189ac4337b4d
ZIS-SUN <user@example.com>
2025-11-28 14:43:34 +0800

    Add Vercel configuration for backend deployment

--- backend/models/User.js
+++ backend/models/User.js
@@ -26,13 +26,12 @@ const userSchema = new mongoose.Schema({
 });
 
 // Hash password before saving
-userSchema.pre('save', async function(next) {
+userSchema.pre('save', async function() {
   if (!this.isModified('password')) {
-    return next();
+    return;
   }
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
-  next();
 });
 
 // Compare password method
--- backend/routes/blogRoutes.js
+++ backend/routes/blogRoutes.js
@@ -14,16 +14,16 @@ const {
 } = require('../controllers/commentController');
 const { protect } = require('../middleware/authMiddleware');
 
+// Comment routes (must be before /:id routes to avoid conflicts)
+router.get('/:postId/comments', getComments);
+router.post('/:postId/comments', protect, createComment);
+router.delete('/:postId/comments/:commentId', protect, deleteComment);
+
 // Blog post routes
 router.get('/', getBlogPosts);
 router.get('/:id', getBlogPostById);
 router.post('/', protect, createBlogPost);
 router.put('/:id', protect, updateBlogPost);
 router.delete('/:id', protect, deleteBlogPost);
-
-// Comment routes
-router.get('/:postId/comments', getComments);
-router.post('/:postId/comments', protect, createComment);
-router.delete('/:postId/comments/:commentId', protect, deleteComment);
 
 module.exports = router;
